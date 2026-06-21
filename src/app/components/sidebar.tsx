1cf6040e02ec185418fc93a4994f81f38f31eed2
RafaelLfckkj <rafaxolive@gmail.com>
2026-06-21 15:29:06 -0300

    estrutura do next | SideBar quase finalizada | fluxo de pagias fucionando

---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUser,
+  faLightbulb,
+  faEnvelope,
+  faEarthAmericas,
+} from "@fortawesome/free-solid-svg-icons";
+
+
+import Link from 'next/link';
+import Image from "next/image";
+
+export default function Sidebar() {
+  return (
+    <div className="fixed left-16 top-1/2 -translate-y-1/2 bg-[#05131E] text-white w-16 flex flex-col items-start justify-between py-6 px-5 rounded-3xl hover:w-3xs duration-700 ease-in-out overflow-hidden group">
+      <div className="mb-10">
+        logo...
+      </div>
+      <div className="grid gap-10 ">
+        <ul className="grid gap-10 ">
+          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg">
+            <FontAwesomeIcon icon={faHouse} size="lg" />
+            <a className="hidden group-hover:block ml-2 " href="/">Home</a>
+          </li>
+          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg">
+            <FontAwesomeIcon icon={faUser} size="lg" />
+            <a className="hidden group-hover:block ml-2 whitespace-nowrap" href="/sobre">Sobre Mim</a>
+          </li>
+          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg">
+            <FontAwesomeIcon icon={faLightbulb} size="lg" />
+            <a className="hidden group-hover:block ml-2" href="/projetos">Projetos</a>
+          </li>
+          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg">
+            <FontAwesomeIcon icon={faEnvelope} size="lg" />
+            <a className="hidden group-hover:block ml-2" href="/contato">Contato</a>
+          </li>
+        </ul>
+      </div>
+      <div className="mt-50 flex">
+        <FontAwesomeIcon icon={faEarthAmericas} size="lg" />
+        <a className="hidden group-hover:block ml-2  rounded-lg whitespace-nowrap" href="">Trocar Idioma</a>
+      </div>
+    </div>
+  );
+}
