5b2313b0e0a6062788aa4332b93ce27b92066bd1
RafaelLfckkj <rafaxolive@gmail.com>
2026-06-27 14:09:36 -0300

    Fazendo ajustes na sideBar

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -9,35 +9,42 @@ import {
 
 
 import Link from 'next/link';
-import Image from "next/image";
+
 
 export default function Sidebar() {
   return (
     <div className="fixed left-16 top-1/2 -translate-y-1/2 bg-[#05131E] text-white w-16 flex flex-col items-start justify-between py-6 px-5 rounded-3xl hover:w-3xs duration-700 ease-in-out overflow-hidden group">
       <div className="mb-10">
-        <Image src="/logo.png" alt="Logo temporaria" width={40} height={40} />
+        <span className="text-[#7ED8BF]  -ml-2 text-lg font-mono font-bold group-hover:hidden">
+          {"<R/>"}
+        </span> 
+
+        <span className="hidden group-hover:block text-[#7ED8BF] text-lg font-mono font-bold whitespace-nowrap">
+          {"<Rafael/>"}
+        </span>
       </div>
       <div className="grid gap-10 ">
         <ul className="grid gap-10 ">
-          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg">
+          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg hover:p-2 duration-200">
+            
             <FontAwesomeIcon icon={faHouse} size="lg" />
             <Link className="hidden group-hover:block ml-2 " href="/">
               Home
             </Link>
           </li>
-          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg">
+          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg hover:p-2 duration-200">
             <FontAwesomeIcon icon={faUser} size="lg" />
             <Link className="hidden group-hover:block ml-2 whitespace-nowrap" href="/sobre">
               Sobre Mim
             </Link>
           </li>
-          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg">
+          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg hover:p-2 duration-200">
             <FontAwesomeIcon icon={faLightbulb} size="lg" />
             <Link className="hidden group-hover:block ml-2" href="/projetos">
               Projetos
             </Link>
           </li>
-          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg">
+          <li className="flex hover:bg-[#1C2D3A] hover:w-full rounded-lg hover:p-2 duration-200">
             <FontAwesomeIcon icon={faEnvelope} size="lg" />
             <Link className="hidden group-hover:block ml-2" href="/contato">
               Contato
